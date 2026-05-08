3333e0ed676b2498609b13b3b3f1f23196f1c8f8
tmarfai <marfaitomi28@gmail.com>
2026-05-08 08:57:53 +0200

    kultik szöveg added

--- script.js
+++ script.js
@@ -703,6 +703,10 @@ function initSmartNearbyExplorer() {
     // Ellenőrizzük, hogy a Kultik Mozit választották-e ki (név alapján)
     if (place.name.toLowerCase().includes("kultik")) {
       summaryHtml += `
+      <div class="mt-3 pt-2 border-top">
+          <p class="mb-2" style="font-size: 0.9rem; color: #1b447d; font-weight: 600;">
+            A képre kattintva megtekintheti az aktuális moziműsort:
+          </p>
         <div class="mt-2">
           <a href="https://kaposvarimozi.hu/" target="_blank">
             <img src="kultik_logo.png" alt="Kultik Mozi" style="max-width: 120px; cursor: pointer; border-radius: 8px; transition: transform 0.2s;">
